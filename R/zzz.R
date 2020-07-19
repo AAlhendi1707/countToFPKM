acfc0488a7967e5fb1e89b740a94ff6a09a91455
Ahmed Alhendi <44529285+AAlhendi1707@users.noreply.github.com>
2020-07-19 11:25:27 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,9 @@
 .onAttach <- function(libname, pkgname) {
- cat("=============================================================================================\n")
- cat("For citation of countToFPKM, please use:\n")
- cat("Alhendi, A.S.N. (2019). countToFPKM: Convert Counts to Fragments per Kilobase of Transcript per Million (FPKM). R package version 1.0.0, : https://CRAN.R-project.org/package=countToFPKM\n")
- cat("=============================================================================================\n")
-  }
-
-.onLoad <- function(libname, pkgname) {
- cat("=============================================================================================\n")
- cat("For citation of countToFPKM, please use:\n")
- cat("Alhendi, A.S.N. (2019). countToFPKM: Convert Counts to Fragments per Kilobase of Transcript per Million (FPKM). R package version 1.0.0, : https://CRAN.R-project.org/package=countToFPKM\n")
- cat("=============================================================================================\n")
-  }
+ packageStartupMessage(rule())
+    packageStartupMessage(
+      "To cite the R package 'countToFPKM' in publications use:\n",
+      "Alhendi, A.S.N. (2019). countToFPKM: Convert Counts to Fragments per Kilobase of Transcript per Million (FPKM). R package version 1.0.0.\n",
+      "https://CRAN.R-project.org/package=countToFPKM\n"
+    )
+ packageStartupMessage(rule())
+}
